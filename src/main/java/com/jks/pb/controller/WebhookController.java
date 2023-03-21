0cfc48c55dfcce609bb4c983a4b3c7dc418a2c01
zxf <530541683@qq.com>
2023-03-21 16:22:53 +0800

    1

--- src/main/java/com/jks/pb/controller/WebhookController.java
+++ src/main/java/com/jks/pb/controller/WebhookController.java
@@ -3,14 +3,17 @@ package com.jks.pb.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSONObject;
 
 @RestController
 public class WebhookController {
 
 	@RequestMapping("/github-webhook")
-	public void hook(HttpServletRequest request){
+	public void hook(HttpServletRequest request,@RequestBody JSONObject  obj){
 		System.out.println(2);
 		System.out.println(1);
 	}
